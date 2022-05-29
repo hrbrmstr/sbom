1a9b76f8ff8f99d7ef621fbb3524144b78b5469b
hrbrmstr <bob@rud.is>
2022-05-29 12:42:27 -0400

    R package repo initialization complete

---
+++ R/sbom-package.R
@@ -0,0 +1,10 @@
+#' ...
+#' 
+#' @md
+#' @name sbom
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+# @ useDynLib sbom, .registration = TRUE
+"_PACKAGE"
