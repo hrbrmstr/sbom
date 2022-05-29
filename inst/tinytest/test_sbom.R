d2915aec0361ead9e259ca55ba59d6d79618cc9d
hrbrmstr <bob@rud.is>
2022-05-29 13:18:37 -0400

    some mininmal tests

--- inst/tinytest/test_sbom.R
+++ inst/tinytest/test_sbom.R
@@ -1,4 +1,11 @@
+library(sbom)
+library(testthat)
 
 # Placeholder with simple test
-expect_equal(1 + 1, 2)
+x <- cran_pkg_sbom("ndjson")
 
+expect_equal(length(x), 1)
+
+expect_is(x, "json")
+
+expect_true(validate_sbom(text=x))
